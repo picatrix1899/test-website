53d03efee4e1dfd33018b78d6a669d5fd1e085c0
picatrix1899 <FlorianZilkenat@gmx.de>
2019-02-14 18:38:26 +0100

    .

--- home.md
+++ home.md
@@ -1,3 +1,18 @@
 # Home
 
 ## Test
+
+adfasdfasdfasd
+asdfasdf
+asdf
+asdf
+asdf
+
+```java
+string s = "";
+public void test()
+{
+  int i = 0;
+  String test = "lorem ipsum dolore me amed";
+}
+```
--- sitetemplate.php
+++ sitetemplate.php
@@ -71,7 +71,7 @@
         <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
         <script>
            document.getElementById('content').innerHTML =
-           marked(<?php echo json_encode($content) ?>, {gfm: true, breaks: true});
+           marked(<?php echo json_encode($content) ?>, {gfm: true});
         </script>
       </div>
     </div>
