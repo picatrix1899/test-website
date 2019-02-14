e243c375a7c2df2456cc10cbb3633a634e3f7c9b
picatrix1899 <FlorianZilkenat@gmx.de>
2019-02-14 16:15:03 +0100

    .

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <base href="/test-website/">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+
+    <title>FZ</title>
+  </head>
+  <body>
+
+    <div class="main">
+
+      <?php require 'headnav.php' ?>
+
+      <div class="content">
+        <div id="content">
+          <?php
+            
+            $t = 'Hello World';
+
+            include 'test.php'
+
+          ?>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
+        <script>
+          // document.getElementById('content').innerHTML =
+          // marked('# Marked in browser\n\nRendered by **marked**.');
+        </script>
+      </div>
+
+    </div>
+    
+    <div class="left-sidebar">
+        <div class="left-sidenavbar" id="left-sidenavbar">
+            <a href="">Option 1</a>
+            <a href="">Option 2</a>
+            <a href="">Option 3</a>
+            <a href="">Option 4</a>
+            <a href="">Option 5</a>
+        </div>
+
+      </div>
+
+<?php require 'scripts.php'?>
+
+  </body>
+</html>
